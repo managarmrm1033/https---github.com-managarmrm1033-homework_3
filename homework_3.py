6cca51c478db9921b9178d14624ed7cd364c8587
managarmrm1033 <mrtimofei462@gmail.com>
2024-04-20 14:00:55 +0300

    commit(Виправленно)

--- homework_3.py
+++ homework_3.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-     while True:
+     attemtps = 3
+     while attemtps > 0:
           try:
-               if isinstance(date, str):
-                    date = datetime.strptime(date, "%Y.%m.%d").date()
-
+               date = datetime.strptime(date, "%Y.%m.%d").date()
                day_today = datetime.today().date()
-               differense_day = date - day_today # або day_today - date щоб виходило плюсове значення ,а не -90
+               differense_day = day_today - date # або day_today - date щоб виходило плюсове значення ,а не -90
                return differense_day
-          except TypeError:
-               datetime_object = datetime.strptime(date, "%Y.%m.%d")
           except ValueError:
                print("Введіть парвильний формат данних (%Y.%m.%d або 2024.01.21)")
                date = input("Спробуйте будь ласка ще раз:)! ")
+               attemtps -= 1
+          return "Не вдалось ввести коректну дату"
+
 sup = input("Введіть дату: ")
 print(get_days_from_today(sup))
